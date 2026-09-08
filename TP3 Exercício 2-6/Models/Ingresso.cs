d76c6013717091dac6417c55b1bf224747ba09e9
kauasantost007 <kauasantost007@gmail.com>
2026-09-07 23:10:01 -0300

    Add files via upload

---
+++ TP3 Exercício 2-6/Models/Ingresso.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TP3_Exercícios_2a6.Models
+{
+    public class Ingresso
+    {
+        public string nomeDoShow;           /// Identifica a qual evento o ingresso pertence.
+        public double preco;                /// Define o valor cobrado por ingresso.
+        public int quantidadeDisponivel;    /// Controla quantos ingressos ainda podem ser vendidos.
+
+        public Ingresso(string nomeDoShow, double preco, int quantidadeDisponivel)
+        {
+            this.nomeDoShow = nomeDoShow;
+            this.preco = preco;
+            this.quantidadeDisponivel = quantidadeDisponivel;
+        }
+
+        public void AlterarPreco(double novoPreco)
+        {
+            preco = novoPreco;
+        }
+
+        public void AlterarQuantidade(int novaQuantidade)
+        {
+            quantidadeDisponivel = novaQuantidade;
+        }
+
+        public void ExibirInformacoes()
+        {
+            Console.WriteLine("Show: " + nomeDoShow);
+            Console.WriteLine("Preço: " + preco);
+            Console.WriteLine("Quantidade disponível: " + quantidadeDisponivel);
+        }
+        public string GetNomeDoShow()
+        {
+            return nomeDoShow;
+        }
+
+        public double GetPreco()
+        {
+            return preco;
+        }
+
+        public int GetQuantidadeDisponivel()
+        {
+            return quantidadeDisponivel;
+        }
+
+        public void SetNomeDoShow(string novoNome)
+        {
+            nomeDoShow = novoNome;
+        }
+
+        public void SetPreco(double novoPreco)
+        {
+            preco = novoPreco;
+        }
+
+        public void SetQuantidadeDisponivel(int novaQtd)
+        {
+            quantidadeDisponivel = novaQtd;
+        }
+    }
+}
---
+++ TP3 Exercício 2-6/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using TP3_Exercícios_2a6.Models;
+
+namespace TP3_Exercícios_2a6
+/*
+    /// Exercício 4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingresso showTravis = new Ingresso();
+            showTravis.nomeDoShow = "Travis Scott - Circus Maximus Tour";
+            showTravis.preco = 320.00;
+            showTravis.quantidadeDisponivel = 600;
+
+            Console.WriteLine("--- Informações iniciais ---");
+            showTravis.ExibirInformacoes();
+
+            showTravis.AlterarPreco(360.00);
+            showTravis.AlterarQuantidade(500);
+
+            Console.WriteLine("\n--- Informações após alterações ---");
+            showTravis.ExibirInformacoes();
+        }
+    }
+}
+
+*/
+
+
+/// Exercício 6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ingresso showTravis = new Ingresso("Travis Scott - Circus Maximus Tour", 320.00, 600);
+
+            showTravis.ExibirInformacoes();
+        }
+    }
+}
+
+/// O construtor facilita porque cria o objeto já completo, em uma única linha, obrigando que todos os dados essenciais
+/// sejam informados no momento da criação. Com vários Set... separados, seriam necessárias várias linhas,existe o risco
+/// de esquecer de preencher algum atributo, e o objeto fica temporariamente incompleto entre uma chamada e outra, problemas que o construtor elimina.
